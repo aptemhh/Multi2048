f68dd8ac63b0c19ceff51eca371b41519b8ae35a
Idony <aptemhh@mail.ru>
2016-03-07 19:24:45 +0600

    Создать класс который будет получать значения

---
+++ Multi2048/DvigOnLine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multi2048
+{
+    class DvigOnLine
+    {
+        String s = "";
+        public void Set(Char s, int x, int y, int v)
+        {
+        }
+        public String Get()
+        {
+            return s;
+        }
+    }
+}
