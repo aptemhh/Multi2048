e3ec17e98519f67e415a436ed687a01bae6c4cea
Idony <aptemhh@mail.ru>
2016-03-09 16:19:45 +0600

    Создать наследника DataGridView с двумя типами Update по сети и по нажатию

---
+++ Multi2048/GamePanel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multi2048
+{
+    class GamePanel : DataGridView
+    {
+        int[,] mas = new int[,] { { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+        Motion dvig;
+        public ScoreGame scopeGame;
+        public void UpdateKey(object sendere, KeyEventArgs ee)
+        {
+            if (dvig != null)
+                mas = dvig.UpdateKey(sendere, ee, mas);
+        }
+        public void UpdateLine(Char s, int x, int y, int v)
+        {
+            if (dvig != null)
+                mas = dvig.UpdateLine(s, x, y, v, mas);
+        }
+        public void SetDvig(Motion _dvig)
+        {
+            dvig = _dvig;
+            dvig.scopeGame += (int i) => {
+                if (scopeGame!=null)
+                scopeGame(i); 
+            };
+        }
+
+    }
+}
