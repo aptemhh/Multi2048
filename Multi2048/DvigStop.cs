57f80a50190ae0367bd6fe618f48016291979a2b
Idony <aptemhh@mail.ru>
2016-03-09 16:16:58 +0600

    Создать двигатель,который ничего не делает

---
+++ Multi2048/DvigStop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multi2048
+{
+    class DvigStop : Motion
+    {
+
+        public override int[,] UpdateLine(Char s, int x, int y, int v, int[,] mas)
+        {
+
+            return mas;
+        }
+        public override int[,] UpdateKey(object sendere, KeyEventArgs ee, int[,] mas)
+        {
+
+            return mas;
+        }
+    }
+}
