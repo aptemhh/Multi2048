fb714b9ba90502940c3f924a0949321f7ee8de00
Idony <aptemhh@mail.ru>
2016-04-17 22:56:01 +0600

    Отредактировать стиль кода DvigOnLine

--- Multi2048/DvigOnLine.cs
+++ Multi2048/DvigOnLine.cs
@@ -1,28 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
+﻿//-----------------------------------------------------------------------
+// <copyright file="DvigOnLine.cs" company="IVT-131">
+// Copyright (c) IVT-131. All rights reserved.
+// </copyright>
+// <author>Лобачев Андрей</author>
+// <author>Супрун Артем</author>
+// <author>Дарий Олеся</author>
+//-----------------------------------------------------------------------
 namespace Multi2048
 {
-    class DvigOnLine : Motion
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// Engine Bluetooth
+    /// </summary>
+    public class DvigOnLine : Motion
     {
-        int scope = 0;
-        public override void UpdateLine(Char s, int x, int y, int v, int[,] mas)
+        /// <summary>
+        /// Updating of the network, by changing the argument mas
+        /// </summary>
+        /// <param name="s">Direction r l u d</param>
+        /// <param name="x">Situation X new figures</param>
+        /// <param name="y">Situation Y new figures</param>
+        /// <param name="v">The new figure</param>
+        /// <param name="mas">Source array</param>
+        public override void UpdateLine(char s, int x, int y, int v, int[,] mas)
         {
-             
         }
+
+        /// <summary>
+        /// Upgrading Using the buttons
+        /// </summary>
+        /// <param name="sendere">The object on which clicked</param>
+        /// <param name="ee">Current press</param>
+        /// <param name="mas">Source array</param>
         public override void UpdateKey(object sendere, KeyEventArgs ee, int[,] mas)
         {
-            if (ee.KeyData == Keys.W)
-            {
-
-            }
-            if (scopeGame != null)
-                scopeGame(scope++);
-            
         }
     }
 }
