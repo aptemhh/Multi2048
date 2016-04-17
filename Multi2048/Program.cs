4349a3bb7c98b83170717fdee94e38f666d040d1
Idony <aptemhh@mail.ru>
2016-04-17 22:57:16 +0600

    Отредактировать стиль кода SocketUniversal

--- Multi2048/Program.cs
+++ Multi2048/Program.cs
@@ -1,18 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="IVT-131">
+// Copyright (c) IVT-131. All rights reserved.
+// </copyright>
+// <author>Лобачев Андрей</author>
+// <author>Супрун Артем</author>
+// <author>Дарий Олеся</author>
+//-----------------------------------------------------------------------
 namespace Multi2048
 {
-    static class Program
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// class Program
+    /// </summary>
+    public static class Program
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        public static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
--- Multi2048/SocketUniversal.cs
+++ Multi2048/SocketUniversal.cs
@@ -1,41 +1,93 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-
+﻿//-----------------------------------------------------------------------
+// <copyright file="SocketUniversal.cs" company="IVT-131">
+// Copyright (c) IVT-131. All rights reserved.
+// </copyright>
+// <author>Лобачев Андрей</author>
+// <author>Супрун Артем</author>
+// <author>Дарий Олеся</author>
+//-----------------------------------------------------------------------
 namespace Multi2048
 {
-    public delegate void StatusGame(String s);//подписка на статус
-    public  delegate void InfoGame(Char s, int x, int y, int v);//подписка на направление и (x,y) цифра
-    class SocketUniversal
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Subscribe to status
+    /// </summary>
+    /// <param name="s">Input status bar</param>
+    public delegate void StatusGame(string s);
+
+    /// <summary>
+    /// Subscription and direction (x, y) number
+    /// </summary>
+    /// <param name="s">Direction r l u d</param>
+    /// <param name="x">x coordinate</param>
+    /// <param name="y">y coordinate</param>
+    /// <param name="v">New figure</param>
+    public delegate void InfoGame(char s, int x, int y, int v);
+
+    /// <summary>
+    /// Universal socket for connection
+    /// </summary>
+    public class SocketUniversal
     {
-        //Socket s;
-        public StatusGame MyEvent; // Старт 1-3
-        public InfoGame MyEvent2; // Событие
-        public SocketUniversal()//(Socket _s){s=_s;
-        {
+        /// <summary>
+        /// Delegate game status
+        /// </summary>
+        private StatusGame statusGame;
 
+        /// <summary>
+        /// Delegate information about the game to the opponent
+        /// </summary>
+        private InfoGame infoGame;
+
+        /// <summary>
+        /// Sets delegate game status
+        /// </summary>
+        public StatusGame StatusGame
+        {
+            set { this.statusGame += value; }
         }
-        public void run()// поток, который читает сокет
-        {
-            for (; ; )
-            {
-                    if (MyEvent != null)
-                        MyEvent("статус");
-                    if (MyEvent2 != null)//направление
-                        MyEvent2('R', 0, 0, 2);
 
-                //чтение, обработка
-                
+        /// <summary>
+        /// Sets delegate information about the game to the opponent
+        /// </summary>
+        public InfoGame InfoGame
+        {
+            set { this.infoGame += value; }
+        }
+
+        /// <summary>
+        /// Stream that reads socket
+        /// </summary>
+        public void Run() // поток, который читает сокет
+        {
+            for (;;)
+            {
+                if (this.statusGame != null)
+                {
+                    this.statusGame("статус");
+                }
+
+                if (this.infoGame != null)
+                {
+                    this.infoGame('R', 0, 0, 2);
+                }
+
                 Thread.Sleep(1000);
             }
         }
-        public void write(String s)
+
+        /// <summary>
+        /// Writer in the socket stream
+        /// </summary>
+        /// <param name="s">Message to the socket</param>
+        public void Write(string s)
         {
         }
-
-
     }
 }
