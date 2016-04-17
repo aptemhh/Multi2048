2c93f09bcc48af4d02fc1890c21de6284b6335ce
Idony <aptemhh@mail.ru>
2016-04-17 22:56:57 +0600

    Отредактировать стиль кода Motion

--- Multi2048/Motion.cs
+++ Multi2048/Motion.cs
@@ -1,37 +1,83 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
+﻿//-----------------------------------------------------------------------
+// <copyright file="Motion.cs" company="IVT-131">
+// Copyright (c) IVT-131. All rights reserved.
+// </copyright>
+// <author>Лобачев Андрей</author>
+// <author>Супрун Артем</author>
+// <author>Дарий Олеся</author>
+//-----------------------------------------------------------------------
 namespace Multi2048
 {
+    using System;
+    using System.Windows.Forms;
 
-    abstract class Motion//интерфейс двигателя
+    /// <summary>
+    /// Abstract class engines
+    /// </summary>
+    public abstract class Motion
     {
-        public ScoreGame scopeGame;
-        public abstract void UpdateLine(Char s, int x, int y, int v, int[,] mas);
+        /// <summary>
+        /// The delegate of the game account
+        /// </summary>
+        private ScoreGame scopeGame;
+
+        /// <summary>
+        /// Subject to randomness
+        /// </summary>
+        private Random rnd = new Random();
+
+        /// <summary>
+        /// Sets to delegate action
+        /// </summary>
+        public ScoreGame SetScopeGame
+        {
+            set { this.scopeGame += value; }
+        }
+
+        /// <summary>
+        /// Abstract method of updating via the network
+        /// </summary>
+        /// <param name="s">Direction r l u d</param>
+        /// <param name="x">Coordinate X</param>
+        /// <param name="y">Coordinate Y</param>
+        /// <param name="v">The new figure</param>
+        /// <param name="mas">Source array</param>
+        public abstract void UpdateLine(char s, int x, int y, int v, int[,] mas);
+
+        /// <summary>
+        /// Abstract method of updating via the keyboard
+        /// </summary>
+        /// <param name="sendere">The object on which clicked</param>
+        /// <param name="ee">Clicking properties</param>
+        /// <param name="mas">Source array</param>
         public abstract void UpdateKey(object sendere, KeyEventArgs ee, int[,] mas);
-        Random rnd = new Random();
+
+        /// <summary>
+        /// The new figure
+        /// </summary>
+        /// <param name="mas">The original array</param>
         public void NewTile(int[,] mas)
         {
             int cntr = 0;
-
             for (int i = 0; i < 4; i++)
-                for (int j = 0; j < 4; j++)
-                    if (mas[i, j] == 0)
-                        cntr++;
-
-            for (int n = cntr; cntr > 0 && ((cntr - n) < 3) && n > 0; )
             {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (mas[i, j] == 0)
+                    {
+                        cntr++;
+                    }
+                }
+            }
 
+            for (int n = cntr; cntr > 0 && ((cntr - n) < 3) && n > 0;)
+            {
                 int x, y;
-                x = rnd.Next(0, 4);
-                y = rnd.Next(0, 4);
+                x = this.rnd.Next(0, 4);
+                y = this.rnd.Next(0, 4);
                 if (mas[x, y] == 0)
                 {
-                    mas[x, y] = rnd.Next(0, 100) < 90 ? 2 : 4;
+                    mas[x, y] = this.rnd.Next(0, 100) < 90 ? 2 : 4;
                     n--;
                     break;
                 }
@@ -43,60 +89,99 @@ namespace Multi2048
                 }
             }
         }
+
+        /// <summary>
+        /// Moving towards chips
+        /// </summary>
+        /// <param name="a">Party Up Down Left Right</param>
+        /// <param name="mas">Source array</param>
         public void MoveTile(string a, int[,] mas)
         {
             switch (a)
             {
                 case "up":
                     for (int k = 0; k < 3; k++)
+                    {
                         for (int j = 0; j < 4 - 1; j++)
+                        {
                             for (int i = 0; i < 4; i++)
+                            {
                                 if (mas[i, j] == 0)
                                 {
                                     mas[i, j] = mas[i, j + 1];
                                     mas[i, j + 1] = 0;
                                 }
+                            }
+                        }
+                    }
+
                     break;
                 case "down":
                     for (int k = 0; k < 3; k++)
+                    {
                         for (int j = 4 - 1; j >= 1; j--)
+                        {
                             for (int i = 0; i < 4; i++)
+                            {
                                 if (mas[i, j] == 0)
                                 {
                                     mas[i, j] = mas[i, j - 1];
                                     mas[i, j - 1] = 0;
                                 }
+                            }
+                        }
+                    }
+
                     break;
                 case "left":
                     for (int k = 0; k < 3; k++)
+                    {
                         for (int i = 0; i < 4 - 1; i++)
+                        {
                             for (int j = 0; j < 4; j++)
+                            {
                                 if (mas[i, j] == 0)
                                 {
                                     mas[i, j] = mas[i + 1, j];
                                     mas[i + 1, j] = 0;
                                 }
+                            }
+                        }
+                    }
+
                     break;
                 case "right":
                     for (int k = 0; k < 3; k++)
+                    {
                         for (int i = 4 - 1; i >= 1; i--)
+                        {
                             for (int j = 0; j < 4; j++)
+                            {
                                 if (mas[i, j] == 0)
                                 {
                                     mas[i, j] = mas[i - 1, j];
                                     mas[i - 1, j] = 0;
                                 }
+                            }
+                        }
+                    }
+
                     break;
             }
         }
 
+        /// <summary>
+        /// Merge chips
+        /// </summary>
+        /// <param name="a">Direction Up Down Left Right</param>
+        /// <param name="mas">The original array </param>
         public void StackTile(string a, int[,] mas)
         {
-
             switch (a)
             {
                 case "up":
                     for (int j = 0; j < 4 - 1; j++)
+                    {
                         for (int i = 0; i < 4; i++)
                         {
                             int v1, v2;
@@ -105,18 +190,22 @@ namespace Multi2048
                             if (v1 == v2 && v1 != 0)
                             {
                                 v1 = 2 * v2;
-                                scopeGame(v1);
+                                this.scopeGame(v1);
                                 v2 = 0;
                                 mas[i, j] = v1;
                                 mas[i, j + 1] = v2;
                             }
+
                             v1 = 0;
                             v2 = 0;
-                            MoveTile("up", mas);
+                            this.MoveTile("up", mas);
                         }
+                    }
+
                     break;
                 case "down":
                     for (int j = 4 - 1; j >= 1; j--)
+                    {
                         for (int i = 0; i < 4; i++)
                         {
                             int v1, v2;
@@ -125,17 +214,20 @@ namespace Multi2048
                             if (v1 == v2 && v1 != 0)
                             {
                                 v1 = 2 * v2;
-                                scopeGame(v1);
+                                this.scopeGame(v1);
                                 v2 = 0;
                                 mas[i, j] = v1;
                                 mas[i, j - 1] = v2;
                             }
-                            MoveTile("down", mas);
+
+                            this.MoveTile("down", mas);
                         }
+                    }
 
                     break;
                 case "left":
                     for (int i = 0; i < 4 - 1; i++)
+                    {
                         for (int j = 0; j < 4; j++)
                         {
                             int v1, v2;
@@ -144,17 +236,20 @@ namespace Multi2048
                             if (v1 == v2 && v1 != 0)
                             {
                                 v1 = 2 * v2;
-                                scopeGame(v1);
+                                this.scopeGame(v1);
                                 v2 = 0;
                                 mas[i, j] = v1;
                                 mas[i + 1, j] = v2;
                             }
-                            MoveTile("left", mas);
+
+                            this.MoveTile("left", mas);
                         }
+                    }
 
                     break;
                 case "right":
                     for (int i = 4 - 1; i >= 1; i--)
+                    {
                         for (int j = 0; j < 4; j++)
                         {
                             int v1, v2;
@@ -163,88 +258,41 @@ namespace Multi2048
                             if (v1 == v2 && v1 != 0)
                             {
                                 v1 = 2 * v2;
-                                scopeGame(v1);
+                                this.scopeGame(v1);
                                 v2 = 0;
                                 mas[i, j] = v1;
                                 mas[i - 1, j] = v2;
                             }
-                            MoveTile("right", mas);
+
+                            this.MoveTile("right", mas);
                         }
+                    }
 
                     break;
             }
         }
+
+        /// <summary>
+        /// The logic of the merger and the move
+        /// </summary>
+        /// <param name="a">Direction Up Down Left Right</param>
+        /// <param name="mas">Source array</param>
         public void DO(string a, int[,] mas)
         {
             int[,] masOld = (int[,])mas.Clone();
-            MoveTile(a, mas);
-            StackTile(a, mas);
+            this.MoveTile(a, mas);
+            this.StackTile(a, mas);
             for (int i = 0; i < 4; i++)
+            {
                 for (int j = 0; j < 4; j++)
                 {
                     if (mas[i, j] != masOld[i, j])
                     {
-                        NewTile(mas);
+                        this.NewTile(mas);
                         return;
                     }
                 }
+            }
         }
-        /*
-        void MoveAvailableLeft(int[,] mas)
-        {
-            int cntl = 0;
-
-            for (int i = 0; i < 4 - 1; i++)
-                for (int j = 0; j < 4; j++)
-                    if ((mas[i, j] != 0 && mas[i + 1, j] != mas[i, j]) || mas[i, j] == 0 && mas[i + 1, j] == 0)
-                        cntl++;
-            if (cntl == 12)
-                button3.Enabled = false;
-            else button3.Enabled = true;
-
-        }
-
-        void MoveAvailableRight(int[,] mas)
-        {
-            int cntr = 0;
-
-            for (int i = 4 - 1; i > 0; i--)
-                for (int j = 0; j < 4; j++)
-                    if ((mas[i, j] != 0 && mas[i - 1, j] != mas[i, j]) || mas[i, j] == 0 && mas[i - 1, j] == 0)
-                        cntr++;
-            if (cntr == 12)
-                button4.Enabled = false;
-            else button4.Enabled = true;
-
-        }
-
-        void MoveAvailableUp(int[,] mas)
-        {
-            int cntu = 0;
-
-            for (int j = 0; j < 4 - 1; j++)
-                for (int i = 0; i < 4; i++)
-                    if ((mas[i, j] != 0 && mas[i, j + 1] != mas[i, j]) || mas[i, j] == 0 && mas[i, j + 1] == 0)
-                        cntu++;
-            if (cntu == 12)
-                StateGame("UpDenied");
-            else button2.Enabled = true;
-
-        }
-
-        void MoveAvailableDown(int[,] mas)
-        {
-            int cntd = 0;
-
-            for (int j = 4 - 1; j > 0; j--)
-                for (int i = 0; i < 4; i++)
-                    if ((mas[i, j] != 0 && mas[i, j - 1] != mas[i, j]) || mas[i, j] == 0 && mas[i, j - 1] == 0)
-                        cntd++;
-            if (cntd == 12)
-                button5.Enabled = false;
-            else button5.Enabled = true;
-
-        }
-        */
     }
 }
