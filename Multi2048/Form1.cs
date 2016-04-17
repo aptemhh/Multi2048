119a02ad830174ba731e1a1f7423290e78fee202
Idony <aptemhh@mail.ru>
2016-04-17 22:56:33 +0600

    Отредактировать стиль кода Form1

--- Multi2048/Form1.cs
+++ Multi2048/Form1.cs
@@ -1,105 +1,149 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
+﻿//-----------------------------------------------------------------------
+// <copyright file="Form1.cs" company="IVT-131">
+// Copyright (c) IVT-131. All rights reserved.
+// </copyright>
+// <author>Лобачев Андрей</author>
+// <author>Супрун Артем</author>
+// <author>Дарий Олеся</author>
+//-----------------------------------------------------------------------
 namespace Multi2048
 {
-    public delegate void ScoreGame(int count);//подписка на счет
-    public delegate void StateGame(String s);//подписка на счет
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+ 
+    /// <summary>
+    /// Delegate game account
+    /// </summary>
+    /// <param name="count">Number of points</param>
+    public delegate void ScoreGame(int count);
+
+    /// <summary>
+    /// Game status
+    /// </summary>
+    /// <param name="s">The status bar</param>
+    public delegate void StateGame(string s);
+
+    /// <summary>
+    /// Game form
+    /// </summary>
     public partial class Form1 : Form
     {
+        /// <summary>
+        /// The first game pad
+        /// </summary>
+        private GamePanel gamePanel1;
+
+        /// <summary>
+        /// The object for the network information
+        /// </summary>
+        private SocketUniversal socketUniversal;
+
+        /// <summary>
+        /// The stream for reading socket
+        /// </summary>
+        private Thread socketUniversalThread;
+
+        /// <summary>
+        /// Caption account
+        /// </summary>
+        private Label label;
+
+        /// <summary>
+        /// Counter account
+        /// </summary>
+        private int t = 0;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Form1"/> class.
+        /// </summary>
         public Form1()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
-        SocketUniversal socketUniversal;
-       
-        Thread a;
-        GamePanel gamePanel1;
-        Label label;
-        private void Form1_Load(object sender, EventArgs e)
+        
+        /// <summary>
+        /// Initialization ds
+        /// </summary>
+        public void Init()
         {
-            init();
-            subscribe();
-        }
-        public void status(String s)
-        {
+            this.socketUniversal = new SocketUniversal();
+            this.gamePanel1 = new GamePanel();
+            this.gamePanel1.Init();
+            this.gamePanel1.SetDvig(new DvigWASD());
+            this.gamePanel1.Location = new System.Drawing.Point(269, 73);
+            this.label = new Label();
+            this.label.Location = new System.Drawing.Point(10, 10);
+            this.label.Text = "0";
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.gamePanel1);
         }
 
-        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        /// <summary>
+        /// Subscription all
+        /// </summary>
+        public void Subscribe()
         {
-            a.Abort();
-        }
-        int t = 0;
-        public void init()
-        {
-            socketUniversal = new SocketUniversal();
-            gamePanel1 = new GamePanel();
-            gamePanel1.init();
-            gamePanel1.SetDvig(new DvigWASD());
-            gamePanel1.Location = new System.Drawing.Point(269, 73);
-
-            label = new Label();
-            label.Location = new System.Drawing.Point(10, 10);
-            label.Text = "0";
-            Controls.Add(label);
-
-            Controls.Add(gamePanel1);
-        }
-        public void subscribe()
-        {
-            a = new Thread(socketUniversal.run);
-            a.Start();//запуск чтения сокета
-
-            gamePanel1.infoPanel += (Char s, int x, int y, int v) =>
+            this.socketUniversalThread = new Thread(this.socketUniversal.Run);
+            this.socketUniversalThread.Start(); // запуск чтения сокета
+            this.gamePanel1.InfoPanel = (char s, int x, int y, int v) =>
             {
-                socketUniversal.write("sdfsdf");//сообщение состояния панели соперника
+                this.socketUniversal.Write("sdfsdf"); // сообщение состояния панели соперника
             };
-            socketUniversal.MyEvent += (String s) =>
+            this.socketUniversal.StatusGame = (string s) =>
             {
-                status(s);
-            };//подписка на статус
-            
-
-            KeyUp += (object sendere, KeyEventArgs ee) =>
+            }; // подписка на статус
+            this.KeyUp += (object sendere, KeyEventArgs ee) =>
             {
-                if(ee.KeyCode!=Keys.F11)
-                gamePanel1.UpdateKey(sendere, ee);// подписка на клавиатуру
+                if (ee.KeyCode != Keys.F11)
+                {
+                    this.gamePanel1.UpdateKey(sendere, ee); // подписка на клавиатуру
+                }
             };
-
-            socketUniversal.MyEvent2 += (Char s, int x, int y, int v) =>
+            this.socketUniversal.InfoGame = (char s, int x, int y, int v) =>
             {
-                gamePanel1.UpdateLine(s, x, y, v);//подписка на сеть
-                 
+                this.gamePanel1.UpdateLine(s, x, y, v); // подписка на сеть
             };
-
-            gamePanel1.scopeGame += (int i) =>
+            this.gamePanel1.ScopeGame = (int i) =>
             {
-                t +=i;
-                label.Text = t+"";
+                this.t += i;
+                this.label.Text = t.ToString();
             };
-            gamePanel1.stateGame += (String s) =>
+            this.gamePanel1.StateGame = (string s) =>
             {
                 if (s.Equals("DID 2048"))
                 {
-                    gamePanel1.SetDvig(new DvigStop());
+                    this.gamePanel1.SetDvig(new DvigStop());
                 }
             };
         }
 
+        /// <summary>
+        /// Download form
+        /// </summary>
+        /// <param name="sender"> Object sender</param>
+        /// <param name="e"> Button e </param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Init();
+            this.Subscribe();
+        }
 
-          
+        /// <summary>
+        /// Form Closing
+        /// </summary>
+        /// <param name="sender">Object sender</param>
+        /// <param name="e">Button e</param>
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.socketUniversalThread.Abort();
+        }
     }
-  
 }
-
-
-
-
